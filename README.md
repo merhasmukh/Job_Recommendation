d509b61371b53eb488367f22c2790674e3df3c17
mhgn <mhgn0001@gmail.com>
2023-02-25 11:37:41 +0530

    changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Job_Recommendation
-Job Recommendation Using Deep Learning And Python
+- Job Recommendation Using NLP(Natural Language Processing) And Python
+
+# Command For Run Streamlit App
+- streamlit run streamlit_app.py
+
+
+
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from utils import get_naukri_data
+from utils import get_naukari_data
 
 app=Flask(__name__)
 
 
 @app.route("/")
 def main(postion,loc):
-    url_list=get_naukri_data.get_position_url_list(position=postion,location=loc)
+    url_list=get_naukari_data.get_position_url_list(position=postion,location=loc)
     for i in range(2):
-        print(get_naukri_data.get_position_details(url_list[i]))
+        print(get_naukari_data.get_position_details(url_list[i]))
 
    
 
