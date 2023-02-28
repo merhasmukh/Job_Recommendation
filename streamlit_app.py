6d5ae5d651d68b5a3cfb553c29042ec87766c951
mhgn <mhgn0001@gmail.com>
2023-02-28 21:17:16 +0530

    working on streamlit _app.py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 
-st.title("Job recommendation System")
+st.title("Job Recommendation System")
+position=st.text_input("Enter Your Position")
+loc=st.text_input("Enter Your Location")
+st.write(position)
+st.write(loc)
